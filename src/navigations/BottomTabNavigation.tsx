7d5491201697aba084ee580d1b913e32ceb17647
Jersk41 <japarssidik820@gmail.com>
2024-06-21 03:41:06 +0700

    Add new home screen navigation, with reconfigurate eslint and prettier

--- src/navigations/BottomTabNavigation.tsx
+++ src/navigations/BottomTabNavigation.tsx
@@ -7,38 +7,39 @@ import Favorite from '../screens/Favorite'
 
 const Tab = createBottomTabNavigator()
 
-const BottomTabNavigator = (): JSX.Element => (
-    <Tab.Navigator>
-        <Tab.Screen
-        name='Home'
-        component={Home}
-        options={{
-            tabBarIcon: ({ color }) => (
-                <Feather name='home' size={28} color={color} />
-            ),
-            headerShown: false,
-        }}
-        />
-        <Tab.Screen
-        name='Search'
-        component={Search}
-        options={{
-            tabBarIcon: ({ color }) => (
-                <Feather name='search' size={28} color={color} />
-            ),
-            headerShown: false,
-        }}
-        />
-        <Tab.Screen
-        name='Favorite'
-        component={Favorite}
-        options={{
-            tabBarIcon: ({ color }) => (
-                <Feather name='heart' size={28} color={color} />
-            ),
-            headerShown: false,
-        }} />
-    </Tab.Navigator>
+const BottomTabNavigator = (): React.JSX.Element => (
+  <Tab.Navigator>
+    <Tab.Screen
+      name="Home"
+      component={Home}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="home" size={28} color={color} />
+        ),
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="Search"
+      component={Search}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="search" size={28} color={color} />
+        ),
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="Favorite"
+      component={Favorite}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="heart" size={28} color={color} />
+        ),
+        headerShown: false,
+      }}
+    />
+  </Tab.Navigator>
 )
 
 export default BottomTabNavigator
