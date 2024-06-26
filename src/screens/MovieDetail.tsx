4cb8365d93f6e6b5136ea61a686de984753d69cd
Jersk41 <japarssidik820@gmail.com>
2024-06-26 11:00:50 +0700

    Fix movie detail bug

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -37,18 +37,4 @@ const MovieDetail = ({ navigation }: any): any => {
   )
 }
 
-function MovieDetailFunc({ navigation }): React.JSX.Element {
-  return (
-    <View style={{ alignSelf: 'center', marginTop: '50%' }}>
-      <Text style={{ marginBottom: 8 }}>Ini adalah halaman detail movie</Text>
-      <Button
-        title="Back"
-        onPress={() => {
-          navigation.navigate('MovieScreen')
-        }}
-      />
-    </View>
-  )
-}
-
 export default MovieDetail
